0b0e1c44b12972ba79048afe646121ffea863342
mixaevich18@mail.ru <Mihasigor1997>
2026-07-11 00:28:58 +0300

    HomeWork2

---
+++ Lesson2/src/main/java/HomeWork.java
@@ -0,0 +1,22 @@
+public class HomeWork {
+    public static void main(String[] args) {
+            task1();
+            task2();
+    }
+
+    private static void task1() {
+//        вывести на консоль литералы следующих видов:
+//        логический
+//        строковый
+//        целочисленный
+//        литерал типа float
+//        литерал типа double.
+    }
+
+
+    /**
+     * 1. Метод должен посчитать сумму двух чисел a и b и вывести на экран.
+     **/
+    public static void task2() {
+    }
+}
